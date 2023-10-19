a7a2a410103aee02c87b64b34b31230196fc5dc6
IndrajitParmar <parindra231@gmail.com>
2023-10-19 16:01:07 +0530

    Header part created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
-import { useState } from "react";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import RagisterPage from "./pages/RagisterPage";
+import Layout from "./Layout/Layout";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="login" element={<LoginPage />} />
+        <Route path="register" element={<RagisterPage />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
---
+++ client/src/pages/RagisterPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function RagisterPage() {
+  return <div>Ragister page</div>;
+}
+
+export default RagisterPage;
